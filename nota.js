3767cb519468763c366a7a680226f65aee61dc44
Diego Belli <128156867+diego-belli7@users.noreply.github.com>
2024-03-21 19:06:01 -0300

    Add files via upload

---
+++ nota.js
@@ -0,0 +1,18 @@
+let nota = 6
+
+
+if(nota == 6 ) {
+    console.log ('voce esta na média');
+}
+
+else if(nota <= 5) {
+    console.log('voce está de recuperação');
+}
+
+else if (nota == 10) {
+    console.log('voce está Aprovado');
+}
+
+else {
+    console.log('voce está reprovado')
+}
